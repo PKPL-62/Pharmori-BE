50a531ed9c0e99116c79e5c6b15a237740efdab8
Arxeno <rmfbkw04@gmail.com>
2025-05-10 17:21:23 +0700

    fix

--- medicine/urls.py
+++ medicine/urls.py
@@ -4,9 +4,9 @@ from .views import *
 app_name = 'medicine'
 
 urlpatterns = [
-    path('viewall', viewall, name='viewall'),
-    path('create', create, name='create'),
-    path('restock', restock, name='restock'),
-    path('delete/<str:medicine_id>', delete, name='delete'),
+    path('viewall/', viewall, name='viewall'),
+    path('create/', create, name='create'),
+    path('restock/', restock, name='restock'),
+    path('delete/<str:medicine_id>/', delete, name='delete'),
     path("detail/<path:medicine_id>/", detail, name="detail"),
 ]
--- prescription/urls.py
+++ prescription/urls.py
@@ -4,11 +4,11 @@ from .views import *
 app_name = 'prescription'
 
 urlpatterns = [
-    path('viewall', viewall, name='viewall'),
-    path('create', create, name='create'),
-    path('detail/<str:prescription_id>', detail, name='detail'),
-    path('delete/<str:prescription_id>', delete, name='delete'),
-    path('process/<str:prescription_id>', process, name='process'),
-    path('update/<str:prescription_id>', update, name='update'),
-    path('pays/<str:prescription_id>', pays, name='pays'),
+    path('viewall/', viewall, name='viewall'),
+    path('create/', create, name='create'),
+    path('detail/<str:prescription_id>/', detail, name='detail'),
+    path('delete/<str:prescription_id>/', delete, name='delete'),
+    path('process/<str:prescription_id>/', process, name='process'),
+    path('update/<str:prescription_id>/', update, name='update'),
+    path('pays/<str:prescription_id>/', pays, name='pays'),
 ]
